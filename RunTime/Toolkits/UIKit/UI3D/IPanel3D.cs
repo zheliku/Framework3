74caddbdcbfae85c47af9eca8bcb9256ebc45bc7
zheliku <302734905@qq.com>
2025-09-08 02:46:03 +0800

    补充v1.0.0

---
+++ RunTime/Toolkits/UIKit/UI3D/IPanel3D.cs
@@ -0,0 +1,17 @@
+﻿// ------------------------------------------------------------
+// @file       IPanel3D.cs
+// @brief
+// @author     zheliku
+// @Modified   2024-12-12 14:12:47
+// @Copyright  Copyright (c) 2024, zheliku
+// ------------------------------------------------------------
+
+namespace Framework3.Toolkits.UIKit
+{
+    using UnityEngine;
+
+    public interface IPanel3D : IPanel
+    {
+        public Canvas Canvas { get; }
+    }
+}
